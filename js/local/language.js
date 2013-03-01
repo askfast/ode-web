9e159d9e6ba9e225aa05799b3ec782b324c1de36
Cengizism <culusoy@ask-cs.com>
2013-03-01 14:52:34 +0100

    setup for language selection & multilanguage functionality and configuration

---
+++ js/local/language.js
@@ -0,0 +1,137 @@
+
+
+// TODO
+// Language settings
+
+var ui = {
+    en: {
+      login: {
+        header: "Please sign in",
+        placeholder_username: "Please enter your username",
+        placeholder_password: "Your password",
+        label_rememberMe: "Remember Me",
+        button_login: "Login",
+        button_loggingIn: "Logging..",
+        forgot_password: "forgot your password?"
+      }
+    },
+    nl: {
+      login: {
+        header: "Inloggen",
+        placeholder_username: "Vul uw gebruikersnaam in",
+        placeholder_password: "Vul uw wachtwoord in",
+        label_rememberMe: "Onthoud mij",
+        button_login: "Login",
+        button_loggingIn: "Inloggen..",
+        forgot_password: "wachtwoord vergeten?"
+      }     
+    }
+}
+
+
+
+
+/**
+ * Micheal most of the parts are not matching with front-end anymore but you
+ * I think there are still some that you can use.. That's why I leave it like
+ * it is to you
+ */
+
+// var ui = {
+//     en: {
+
+//       login: {
+//         title: "Login",
+//         header: "Login",
+//         label_username: "Please enter your username",
+//         placeholder_username: "Username",
+//         label_password: "Your password",
+//         placeholder_password: "Password",
+//         label_rememberMe: "Remember Me",
+//         button_login: "Login",
+//         button_loggingIn: "Logging.."
+//       },
+
+
+//       error: {
+//         required: "This field is required.",
+//         remote: "Please fix this field.",
+//         email: "Please enter a valid email address.",
+//         url: "Please enter a valid URL.",
+//         date: "Please enter a valid date.",
+//         dateISO: "Please enter a valid date (ISO).",
+//         number: "Please enter a valid number.",
+//         digits: "Please enter only digits.",
+//         creditcard: "Please enter a valid credit card number.",
+//         equalTo: "Please enter the same value again.",
+//         maxlength: "Please enter no more than {0} characters.",
+//         minlength: "Please enter at least {0} characters.",
+//         rangelength: "Please enter a value between {0} and {1} characters long.",
+//         range: "Please enter a value between {0} and {1}.",
+//         max: "Please enter a value less than or equal to {0}.",
+//         min: "Please enter a value greater than or equal to {0}.",
+
+//         messages: {
+//           login: "<strong>Login failed!</strong><br>Wrong username or password."
+//         },
+
+//         ajax: {
+//           noConnection: "Not connected! Verify your network.",
+//           badRequest: "Bad request!",
+//           notFound: "Requested page not found!",
+//           serverError: "Internal server error",
+//           parserError: "Requested JSON parse failed",
+//           timeout: "Timeout error!",
+//           abort: "Ajax request aborted.",
+//           uncaughtError: "Uncaught Error. "
+//         }
+//       }
+//     },
+//     nl: {
+//       login: {
+//         title: "Login",
+//         header: "Login",
+//         label_username: "Vul uw gebruikersnaam in",
+//         placeholder_username: "Gebruikersnaam",
+//         label_password: "Vul uw wachtwoord in",
+//         placeholder_password: "Wachtwoord",
+//         label_rememberMe: "Onthoud mij",
+//         button_login: "Login",
+//         button_loggingIn: "Inloggen.."
+//       },
+//       error: {
+//         required: "Dit is een verplicht veld.",
+//         remote: "Controleer dit veld.",
+//         email: "Vul hier een geldig e-mailadres in.",
+//         url: "Vul hier een geldige URL in.",
+//         date: "Vul hier een geldige datum in.",
+//         dateISO: "Vul hier een geldige datum in (ISO-formaat).",
+//         number: "Vul hier een geldig getal in.",
+//         digits: "Vul hier alleen getallen in.",
+//         creditcard: "Vul hier een geldig creditcardnummer in.",
+//         equalTo: "Vul hier dezelfde waarde in.",
+//         accept: "Vul hier een waarde in met een geldige extensie.",
+//         maxlength: "Vul hier maximaal {0} tekens in.",
+//         minlength: "Vul hier minimaal {0} tekens in.",
+//         rangelength: "Vul hier een waarde in van minimaal {0} en maximaal {1} tekens.",
+//         range: "Vul hier een waarde in van minimaal {0} en maximaal {1}.",
+//         max: "Vul hier een waarde in kleiner dan of gelijk aan {0}.",
+//         min: "Vul hier een waarde in groter dan of gelijk aan {0}.",
+
+//         messages: {
+//           login: "<strong>Inloggen is mislukt!</strong><br>Onjuiste gebruikersnaam of wachtwoord."
+//         },
+
+//         ajax: {
+//           noConnection: "Niet verbonden met Internet! Controleer uw netwerk instellingen.",
+//           badRequest: "Ongeldig verzoek!",
+//           notFound: "De verzochte pagina wordt niet gevonden!",
+//           serverError: "Interne server foutmelding!",
+//           parserError: "Het is niet lukt om de verzochte JSON te ontleden!",
+//           timeout: "Timeout foutmelding!",
+//           abort: "Ajax verzoek is afgebroken!",
+//           uncaughtError: "De oorzak van de foutmelding is onbekend. "
+//         }
+//       }         
+//     }
+// }
