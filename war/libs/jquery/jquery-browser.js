c817ff83720b36c9e58bfbabca2911b6fbf46075
Cengiz Ulusoy <culusoy@ask-cs.com>
2013-03-19 10:33:54 +0100

    ie6 & ie7 blocking

--- war/libs/jquery/jquery-browser.js
+++ war/libs/jquery/jquery-browser.js
@@ -1,33 +1,4 @@
 /**
- * Avoid `console` errors in browsers that lack a console
- */
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
-}());
-
-
-/**
- * TODO
- * Have one single place for jQuery stuff!
- * 
  * jQuery $.browser plugin
  * Depreciated since 1.9
  */
@@ -80,18 +51,3 @@ if ( browser.chrome ) {
 jQuery.browser = browser;
  
 })( jQuery, window );
-
-
-
-/**
- * Detect IE version for blocking IE6 and IE7
- */
-if ($.browser.msie && Number($.browser.version[0]) < 8)
-{
-  $('#browser-safe').hide();
-  $('#browser-not-compatible').show();
-  $('body').css({
-    'background': 'none'
-  });
-  throw 'This browser is not supported!'
-}
