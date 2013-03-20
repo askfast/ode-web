67e8d4326a27dfc6bb2d5d26ac425bd6d94ebb30
Cengizism <culusoy@ask-cs.com>
2013-03-20 14:09:42 +0100

    settings grabbing after login & settings for languages finished

--- war/js/controllers/login.js
+++ war/js/controllers/login.js
@@ -10,11 +10,12 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
    */
 	var self = this;
 
+
   /**
    * Set default views
    */
-	
-	if($routeParams.uuid && $routeParams.key) {
+	if ($routeParams.uuid && $routeParams.key)
+  {
 		$scope.views = {
 			changePass : true,
 		};
@@ -22,7 +23,9 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
 			uuid : $routeParams.uuid,
 			key :  $routeParams.key,
 		}
-	} else {
+	}
+  else
+  {
 		$scope.views = {
 			login : true,
 			forgot : false
@@ -95,19 +98,6 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
 
   /**
    * TODO
-   * Remove all those jQuery selectors and use ng's
-   * 
-   * Check browser if blacklisted
-   */
-  // if (self.checkBrowser($config.blacklisted))
-  // {
-  //   $('#loginForm').hide();
-  //   $('#browseHappy').show();
-  // };
-
-
-  /**
-   * TODO
    * use native JSON functions of angular and Store service
    */
   var logindata = angular.fromJson(Storage.get('logindata'));
@@ -129,8 +119,6 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
    * Use cookies for user credentials
    * 
    * Login trigger
-   * 
-   * @return {[type]} [description]
    */
   $scope.login = function()
   {
@@ -138,7 +126,6 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
      * Reset alerts
      */
     $('#alertDiv').hide();
-
     /**
      * Checks
      */
@@ -156,24 +143,20 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
           message: $rootScope.ui.login.alert_fillfiled
         }
       };
-
       /**
        * Put button state back to default
        */
       $('#login button[type=submit]')
         .text('Login')
         .removeAttr('disabled');
-
       return false;     
     };
-
     /**
      * Change button state
      */
     $('#login button[type=submit]')
       .text('Login..')
       .attr('disabled', 'disabled');
-
     /**
      * Save login to localStorage
      */
@@ -182,7 +165,6 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
       password: $scope.logindata.password,
       remember: $scope.logindata.remember
     }));
-
     /**
      * Authorize
      */
@@ -216,14 +198,12 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
             message: $rootScope.ui.login.alert_wrongUserPass
           }
         };
-
         /**
          * Put button state back to default
          */
         $('#login button[type=submit]')
           .text('Login')
           .removeAttr('disabled');
-
         return false;
       }
       /**
@@ -246,8 +226,10 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
 
   /**
    * TODO
+   * 
+   * What happens if preloader stucks?
+   * 
    * Optimize preloader and messages
-   * Use local caceh for returned data
    * 
    * Initialize preloader
    */
@@ -259,12 +241,10 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
     $('#login').hide();
     $('#download').hide();
     $('#preloader').show();
-
     /**
      * Preloader
      */
     self.progress(20, $rootScope.ui.login.loading_User);
-
     /**
      * Get user resources
      */
@@ -275,12 +255,22 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
        * Fill app info container
        */
       $rootScope.app.resources = resources;
-
+      /**
+       * Set user language settings
+       */
+      if (resources.settingsWebPaige != null || 
+          resources.settingsWebPaige != undefined)
+      {
+        $rootScope.changeLanguage(angular.fromJson(resources.settingsWebPaige).user.language);
+      }
+      else
+      {
+        $rootScope.changeLanguage($config.defaults.settingsWebPaige.user.language);
+      }
       /**
        * Preloader
        */
       self.progress(40, $rootScope.ui.login.loading_Message);
-
       /**
        * Get messages
        */
@@ -292,24 +282,20 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
          */
         $rootScope.app.unreadMessages = Messages.unreadCount();
         Storage.session.unreadMessages = Messages.unreadCount();
-
         /**
          * Preloader
          */
         self.progress(60, $rootScope.ui.login.loading_Group);
-
         /**
          * Get groups
          */
         Groups.query()
         .then(function(groups)
         {
-
           /**
            * Preloader
            */
           self.progress(80, $rootScope.ui.login.loading_Members);
-
           /**
            * Compile member calls into one pool
            */
@@ -318,24 +304,20 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
           {
             calls.push(Groups.get(group.uuid));
           });
-
           /**
            * Loop through member calls pool
            */
           $q.all(calls)
           .then(function(result)
           {
-
             /**
              * Preloader
              */
             self.progress(100, $rootScope.ui.login.loading_everything);
-
             /**
              * Make unique list of members
              */
             Groups.uniqueMembers();
-            
             /**
              * Presentations
              */
@@ -344,7 +326,6 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
             $('body').css({
               'background': 'url(../img/bg.jpg) repeat'
             });
-
             /**
              * Redirect to dashboard
              */
@@ -367,13 +348,15 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
 
 
   /**
+   * RE-FACTORY
+   * 
    * TODO
+   * 
    * Make button state change!
    * Finish it!
    * 
    * Forgot password
    */
-  
 	$scope.forgot = function() {
 		$('#forgot button[type=submit]').text('setting ...').attr('disabled', 'disabled');
 		User.password($scope.remember.id).then(function(result) {
@@ -396,13 +379,17 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
 					}
 				};
 			}
-			
 			$('#forgot button[type=submit]').text('change password').removeAttr('disabled');
 		});
 	};
 
 
 
+  /**
+   * RE-FACTORY
+   *
+   * Change password
+   */
 	self.changePass =  function(uuid, newpass, key){
 		User.changePass(uuid, newpass, key).then(function(result){
 			if(result.status == 400 || result.status == 500 || result.status == 409){
@@ -429,6 +416,12 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
 		})
 	}
 
+
+  /**
+   * RE-FACTORY
+   *
+   * Change password
+   */
 	$scope.changePass = function() {
 		/**
 		 * Reset alerts
@@ -437,7 +430,8 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
 		/**
 		 * Checks
 		 */
-		if(!$scope.changeData || !$scope.changeData.newPass || !$scope.changeData.retypePass) {
+		if(!$scope.changeData || !$scope.changeData.newPass || !$scope.changeData.retypePass)
+    {
 			/**
 			 * Inform user
 			 */
@@ -448,14 +442,15 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
 					message : 'Please fill all fields!'
 				}
 			};
-			
 			/**
 			 * Put button state back to default
 			 */
 			$('#changePass button[type=submit]').text('change password').removeAttr('disabled');
 
 			return false;
-		}else if($scope.changeData.newPass != $scope.changeData.retypePass){
+		}
+    else if($scope.changeData.newPass != $scope.changeData.retypePass)
+    {
 			$scope.alert = {
 				changePass : {
 					display : true,
@@ -463,39 +458,28 @@ var loginCtrl = function($rootScope, $config, $location, $q, $scope, Session, Us
 					message : 'Please make the reType password is indentical !'
 				}
 			};
-			
 			$('#changePass button[type=submit]').text('change password').removeAttr('disabled');
-
 			return false;
 		};
-
 		/**
 		 * Change button state
 		 */
 		$('#changePass button[type=submit]').text('changing ...').attr('disabled', 'disabled');
-
 		/**
 		 * Save login to localStorage
 		 */
 		// Storage.add('logindata', angular.toJson({
-			// username : $scope.logindata.username,
-			// password : $scope.logindata.password,
-			// remember : $scope.logindata.remember
+		// 	username : $scope.logindata.username,
+		// 	password : $scope.logindata.password,
+		// 	remember : $scope.logindata.remember
 		// }));
-
 		/**
 		 * Authorize
 		 */
 		self.changePass($scope.changepass.uuid, $md5.process($scope.changeData.newPass), $scope.changepass.key);
-		
 	};
 
-
-
 };
-
-
-
 
 
 /**
@@ -511,12 +495,10 @@ loginCtrl.logout = function($rootScope, $config, $scope, $window, Session, User,
    */
   $('.navbar').hide();
   $('#footer').hide();
-
   /**
    * Get logindata
    */
   var logindata = angular.fromJson(Storage.get('logindata'));
-
   /**
    * Log user out
    */
@@ -524,22 +506,18 @@ loginCtrl.logout = function($rootScope, $config, $scope, $window, Session, User,
 	.then(function()
 	{
 		//Session.clear();
-    
     /**
      * Clear localStorage
      */
 		Storage.clearAll();
-
     /**
      * Clear sessionStorage
      */
     Storage.session.clearAll();
-
     /**
      * Set logindata back
      */
     Storage.add('logindata', angular.toJson(logindata));
-
     /**
      * Rediret back to login
      */
@@ -547,67 +525,6 @@ loginCtrl.logout = function($rootScope, $config, $scope, $window, Session, User,
 	});
 };
 
-
-
-
-
-/**
- * Login prototypes
- */
-loginCtrl.prototype = {
-
-  constructor: loginCtrl,
-
-  /**
-   * TODO
-   * Add browser version detection as well..
-   * Still issues with targeting some versions of IE!
-   * 
-   * Needs fixing!
-   * 
-   * Check browser
-   * @param  {[type]} blacklisted [description]
-   * @return {[type]}             [description]
-   */
-  checkBrowser: function(blacklisted)
-  {
-    var N = navigator.appName,
-        ua = navigator.userAgent,
-        tem;
-    var browser = ua.match( /(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i );
-    if (browser && ( tem = ua.match(/version\/([\.\d]+)/i)) != null )
-    {
-      browser[2] = tem[1];
-    }
-    browser = browser ? [ browser[1], browser[2] ] : [ N, navigator.appVersion, '-?' ];
-    browser = browser[0].toLowerCase();
-
-    /**
-     * TODO
-     * 
-     * REMOVE
-     * Remove underscore library dependency here!!
-     * 
-     * @type {Boolean}
-     */
-    return ( _.contains(blacklisted, browser) ) ? true : false;
-
-    /*
-     * Underscore contains function
-     */    
-    // // Determine if the array or object contains a given value (using `===`).
-    // // Aliased as `include`.
-    // _.contains = _.include = function(obj, target) {
-    //   if (obj == null) return false;
-    //   if (nativeIndexOf && obj.indexOf === nativeIndexOf) return obj.indexOf(target) != -1;
-    //   return any(obj, function(value) {
-    //     return value === target;
-    //   });
-    // };
-
-  }
-
-};
 
 loginCtrl.$inject = [ '$rootScope', 
                       '$config', 
