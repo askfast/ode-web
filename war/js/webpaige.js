6f80617853f3a97f9a2b5aa1a6d4fb4409d365ed
Cengizism <culusoy@ask-cs.com>
2013-04-26 11:15:17 +0200

    hotfix for localstorage error on ie7 and lower

--- war/js/webpaige.js
+++ war/js/webpaige.js
@@ -53,28 +53,29 @@ angular.module('WebPaige',[
  * Fetch libraries with AMD (if they are not present) and save in localStorage
  * If a library is presnet it wont be fetched from server
  */
-basket
-  .require(
-    { url: 'libs/chosen/chosen.jquery.min.js' },
-    { url: 'libs/chaps/timeline/2.4.0/timeline_modified.min.js' },
-    { url: 'libs/bootstrap-datepicker/bootstrap-datepicker.min.js' },
-    { url: 'libs/bootstrap-timepicker/bootstrap-timepicker.min.js' },
-    { url: 'libs/daterangepicker/1.1.0/daterangepicker.min.js' },
-    { url: 'libs/sugar/1.3.7/sugar.min.js' },
-    { url: 'libs/raphael/2.1.0/raphael-min.js' }
-  )
-  .then(function ()
-    {
-      basket
-        .require(
-          { url: 'libs/g-raphael/0.5.1/g.raphael-min.js' },
-          { url: 'libs/g-raphael/0.5.1/g.pie-min.js' }
-        )
-        .then(function ()
-        {
-          // console.warn('basket parsed scripts..');
-      });
-    }
-  );
-
-  console.warn('reload is working');
+if ('localStorage' in window && window['localStorage'] !== null)
+{
+  basket
+    .require(
+      { url: 'libs/chosen/chosen.jquery.min.js' },
+      { url: 'libs/chaps/timeline/2.4.0/timeline_modified.min.js' },
+      { url: 'libs/bootstrap-datepicker/bootstrap-datepicker.min.js' },
+      { url: 'libs/bootstrap-timepicker/bootstrap-timepicker.min.js' },
+      { url: 'libs/daterangepicker/1.1.0/daterangepicker.min.js' },
+      { url: 'libs/sugar/1.3.7/sugar.min.js' },
+      { url: 'libs/raphael/2.1.0/raphael-min.js' }
+    )
+    .then(function ()
+      {
+        basket
+          .require(
+            { url: 'libs/g-raphael/0.5.1/g.raphael-min.js' },
+            { url: 'libs/g-raphael/0.5.1/g.pie-min.js' }
+          )
+          .then(function ()
+          {
+            // console.warn('basket parsed scripts..');
+        });
+      }
+    );
+}
