9ded6c34f15cd71965dca97df8a1cd45c2f64ea5
Cengizism <culusoy@ask-cs.com>
2013-04-29 10:32:57 +0200

    latest changes before ie font fixes

--- war/src/plugins.js
+++ war/src/plugins.js
@@ -104,7 +104,22 @@ if ($.browser.msie)
   var ver = $.browser.version || $.browser.version[0];
 
   if (ver == '6.0' || ver == '7.0') window.location = 'browsers.html';
-};/**
+
+  // Reload stylesheet on document ready if IE8
+  // if (ver == '8.0')
+  // {
+  //   $(function ()
+  //   {
+  //     alert('here it comes..');
+
+  //     var $ss = $('#base-css');
+  //     $ss[0].href = $ss[0].href;
+  //   });
+  // }
+}
+
+
+;/**
  * jQuery $.os plugin
  *
  * Author: Cengiz Ulusoy
