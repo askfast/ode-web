1ce12ef98a7ef9b16e3f86b2c8b2a273adcf069d
Cengizism <culusoy@ask-cs.com>
2013-05-13 12:06:35 +0200

    icon fonts are hidden for ie8 & quick fix for tab-content balance

--- war/src/app.js
+++ war/src/app.js
@@ -1365,41 +1365,41 @@ angular.module('WebPaige')
      */
     $rootScope.fixStyles = function ()
     {
-      // var tabHeight = $('.tabs-left .nav-tabs').height();
+      var tabHeight = $('.tabs-left .nav-tabs').height();
 
-      // $.each($('.tab-content').children(), function () 
-      // {
-      //   var $parent = $(this),
-      //       $this = $(this).attr('id'),
-      //       contentHeight = $('.tabs-left .tab-content #' + $this).height();
+      $.each($('.tab-content').children(), function () 
+      {
+        var $parent = $(this),
+            $this = $(this).attr('id'),
+            contentHeight = $('.tabs-left .tab-content #' + $this).height();
 
-      //   /**
-      //    * TODO
-      //    * 
-      //    * Append left border fix
-      //    */
-      //   // $parent.append('<div class="left-border-fix"></div>');
-      //   // console.log('parent ->', $parent);
-      //   // $('#' + $this + ' .left-border-fix').css({
-      //   //   height: contentHeight
-      //   // });
-      //   /**
-      //    * Check if one is bigger than another
-      //    */
+        /**
+         * TODO
+         * 
+         * Append left border fix
+         */
+        // $parent.append('<div class="left-border-fix"></div>');
+        // console.log('parent ->', $parent);
+        // $('#' + $this + ' .left-border-fix').css({
+        //   height: contentHeight
+        // });
+        /**
+         * Check if one is bigger than another
+         */
 
-      //   if (tabHeight > contentHeight)
-      //   {
-      //     // console.log('tab is taller than content ->', $this);
-      //     $('.tabs-left .tab-content #' + $this).css({
-      //       height: $('.tabs-left .nav-tabs').height() - 41
-      //     });
-      //   }
-      //   else if (contentHeight > tabHeight)
-      //   {
-      //     // console.log('content is taller than tabs ->', $this);
-      //     // $('.tabs-left .nav-tabs').css( { height: contentHeight } );
-      //   };
-      // });
+        if (tabHeight > contentHeight)
+        {
+          // console.log('tab is taller than content ->', $this);
+          $('.tabs-left .tab-content #' + $this).css({
+            height: $('.tabs-left .nav-tabs').height() - 41
+          });
+        }
+        else if (contentHeight > tabHeight)
+        {
+          // console.log('content is taller than tabs ->', $this);
+          // $('.tabs-left .nav-tabs').css( { height: contentHeight } );
+        };
+      });
 
       /**
        * Correct icon-font-library icons for mac and linux
