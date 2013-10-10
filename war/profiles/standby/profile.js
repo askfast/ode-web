bc53046b78d38a44ceb5a994a9c4c08024c507e3
Cengizism <culusoy@ask-cs.com>
2013-10-10 09:16:48 +0200

    new profile added

---
+++ war/profiles/standby/profile.js
@@ -0,0 +1,67 @@
+/**
+ * Installation profile
+ */
+var profile = {
+
+  meta: 'isparaat',
+
+  title: 'BRANDWEER',
+
+	host: function ()
+	{
+    return ($.browser.msie) ? '/proxy/standby' : 'http://backend.ask-cs.com/standby';
+	},
+
+  states: [
+    'com.ask-cs.State.Available',
+    'com.ask-cs.State.KNRM.BeschikbaarNoord',
+    'com.ask-cs.State.KNRM.BeschikbaarZuid',
+    'com.ask-cs.State.Unavailable',
+    'com.ask-cs.State.KNRM.SchipperVanDienst',
+    'com.ask-cs.State.Unreached'
+  ],
+
+  timeline: {
+    config: {
+      layouts: {
+        groups:   true,
+        members:  true
+      }
+    }
+  },
+
+  divisions: [],
+
+  roles: [
+    {
+      id: 1,
+      label: 'Planner'
+    }, 
+    {
+      id: 2,
+      label: 'Schipper'
+    }, 
+    {
+      id: 3,
+      label: 'Opstapper'
+    }
+  ],
+
+  p2000: {
+    status: true,
+    url:    'http://knrmtest.myask.me/rpc/client/p2000.php',
+    codes:  '0104517'
+  },
+
+  mobileApp: {
+    status:   false,
+    experimental: false
+  },
+
+  analytics: {
+    status: false,
+    code:   function ()
+    {
+    }
+  }
+};
