69ec0f9bf1b89717e2987f0f810efc702266dae9
Cengizism <culusoy@ask-cs.com>
2014-06-19 14:57:00 +0200

    added a new help section (faq) running still with static mockup content

--- war/js/controllers/help.js
+++ war/js/controllers/help.js
@@ -9,70 +9,77 @@ angular.module('WebPaige.Controllers.Help', [])
 /**
  * Help controller
  */
-.controller('help',
-[
-	'$rootScope', '$scope', '$location',
-	function ($rootScope, $scope, $location)
-	{
-    $rootScope.notification.status = false;
+  .controller(
+  'help',
+  [
+    '$rootScope', '$scope', '$location',
+    function ($rootScope, $scope, $location)
+    {
+      $rootScope.notification.status = false;
 
-		/**
-		 * Fix styles
-		 */
-		$rootScope.fixStyles();
-
-
-	  /**
-	   * View setter
-	   */
-	  function setView (hash)
-	  {
-	    $scope.views = {
-	      dashboard:false,
-	      planboard:false,
-	      messages: false,
-	      groups:  	false,
-	      profile:  false,
-	      settings: false,
-	      support: 	false
-	    };
-
-	    $scope.views[hash] = true;
-	  };
+      /**
+       * Fix styles
+       */
+      $rootScope.fixStyles();
 
 
-	  /**
-	   * Switch between the views and set hash accordingly
-	   */
-	  $scope.setViewTo = function (hash)
-	  {
-	    $scope.$watch(hash, function ()
-	    {
-	      $location.hash(hash);
+      /**
+       * View setter
+       */
+      function setView (hash)
+      {
+        $scope.views = {
+          faq: false,
+          dashboard: false,
+          planboard: false,
+          messages: false,
+          groups: false,
+          profile: false,
+          settings: false,
+          support: false
+        };
 
-	      setView(hash);
-	    });
-	  };
-
-
-	  /**
-	   * If no params or hashes given in url
-	   */
-	  if (!$location.hash())
-	  {
-	    var view = 'dashboard';
-
-	    $location.hash('dashboard');
-	  }
-	  else
-	  {
-	    var view = $location.hash();
-	  }
+        $scope.views[hash] = true;
+      }
 
 
-	  /**
-	   * Set view
-	   */
-	  setView(view);
-	}
-]);
+      /**
+       * Switch between the views and set hash accordingly
+       */
+      $scope.setViewTo = function (hash)
+      {
+        $scope.$watch(
+          hash,
+          function ()
+          {
+            $location.hash(hash);
+
+            setView(hash);
+          }
+        );
+      };
+
+
+      /**
+       * If no params or hashes given in url
+       */
+      var view;
+
+      if (! $location.hash())
+      {
+        view = 'dashboard';
+
+        $location.hash('dashboard');
+      }
+      else
+      {
+        view = $location.hash();
+      }
+
+
+      /**
+       * Set view
+       */
+      setView(view);
+    }
+  ]);
