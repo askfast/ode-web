c97e79b7fbaf2700d64dfff819cdc00aa0b1f81c
Cengiz Ulusoy <culusoy@ask-cs.com>
2014-08-07 13:30:48 +0200

    fix for download button positions

--- war/src/app.js
+++ war/src/app.js
@@ -6476,6 +6476,17 @@ angular.module('WebPaige.Directives', ['ngResource'])
       templateUrl: 'dist/views/download-mobile-app.html',
       link: function (scope, element, attrs)
       {
+        scope.upwards = $.browser.mobile && $.browser.screen.width < 768;
+
+        if ($.browser.mozilla && $.browser.version == '11.0')
+        {
+          angular.element('.download-button')
+            .css(
+            {
+              paddingLeft: '25px'
+            }
+          );
+        }
       }
     };
 
