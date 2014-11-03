43c65f7f17a405a5c93f66c7605e10643f0b06dc
lvdinten <lvdinten@ask-cs.com>
2014-11-03 15:06:56 +0100

    builded version

---
+++ dist/scripts/directives/dashboard/presence-overview.js
@@ -0,0 +1,1 @@
+define(["directives/directives"],function(e){e.directive("presenceOverview",function(){return{restrict:"EA",templateUrl:"views/dashboard/presence-overview.html",link:function(e,t,n){}}})});
